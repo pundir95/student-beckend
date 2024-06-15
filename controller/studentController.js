bb754b7cca2a18435d38b4b2d45242bc79078303
Gaurav Passi <er.gauravpassi@gmail.com>
2024-06-15 15:26:40 +0530

    Created delete and edit

--- controller/studentController.js
+++ controller/studentController.js
@@ -23,7 +23,44 @@ async function getStudent(req,res){
     }
 }
 
+
+async function getStudentById(req, res) {
+    try {
+        const student = await Student.findById(req.params.id);
+        if (!student) return res.status(404).json({ message: 'Student not found' });
+        res.status(200).json(student);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+async function updateStudent(req, res) {
+    try {
+        const updatedStudent = await Student.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedStudent) return res.status(404).json({ message: 'Student not found' });
+        res.status(200).json({
+            message:"Student is updated",
+            data:updatedStudent
+        });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
+
+async function deleteStudent(req, res) {
+    try {
+        const deletedStudent = await Student.findByIdAndDelete(req.params.id);
+        if (!deletedStudent) return res.status(404).json({ message: 'Student not found' });
+        res.json({ message: 'Student deleted successfully' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
 module.exports = {
     createStudent,
-    getStudent
+    getStudent,
+    getStudentById,
+    updateStudent,
+    deleteStudent
 };
--- routes/student.js
+++ routes/student.js
@@ -6,5 +6,8 @@ const studentController=require("../controller/studentController");
 
 router.post('/add',studentController.createStudent)
 router.get('/list',studentController.getStudent)
+router.get('/studentById/:id',studentController.getStudentById)
+router.put('/update/:id',studentController.updateStudent)
+router.delete('/delete/:id',studentController.deleteStudent)
 
 module.exports = router;
